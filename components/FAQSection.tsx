35e2da8481b2f1056e3deb9784c504ae66bf8403
AbdElRahman501 <abdelrahman.6182@gmail.com>
2024-05-06 10:51:43 +0300

    modify name of html files and about and FAQ sections

--- components/FAQSection.tsx
+++ components/FAQSection.tsx
@@ -4,13 +4,26 @@ import FAQCard from "./FAQCard";
 
 const FAQSection = () => {
   return (
-    <section className="max-w-8xl mx-auto p-5 lg:p-20">
+    <section className="mx-auto max-w-8xl p-5 lg:p-20">
       <div className="flex gap-20">
         <div className="flex flex-col gap-4 md:w-1/2">
           <h1 className="text-2xl font-bold uppercase md:text-4xl ">FAQ</h1>
           {faqSection.map((item, index) => (
             <FAQCard key={index} {...item} />
           ))}
+          <FAQCard question="How do I contact customer support?">
+            <p>
+              For any questions or concerns, you can reach our customer support
+              team by emailing{" "}
+              <a
+                className="underline hover:no-underline"
+                href="mailto:support@eh-eg.store"
+              >
+                support@eh-eg.store
+              </a>{" "}
+              . We aim to respond to inquiries promptly.
+            </p>
+          </FAQCard>
         </div>
         <div className="hidden  w-1/2 md:flex">
           <Image
